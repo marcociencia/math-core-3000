2579f9040d1fea7a874ef7ffba0d07c981b503b2
Marco Aurélio Barbosa do Santos <55638752+marcociencia@users.noreply.github.com>
2026-08-10 01:11:29 -0300

    Remove install_requires from setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ ext_modules = [
         ['calculator.cpp'],
         include_dirs=[pybind11.get_include()],
         language='c++',
-        extra_compile_args=['-std=c++17', '-O2'],
+        extra_compile_args=['-std=c++17'],
     ),
 ]
 
@@ -15,5 +15,4 @@ setup(
     name='calc_backend',
     version='1.0',
     ext_modules=ext_modules,
-    install_requires=['pybind11>=2.6.0'],
 )
