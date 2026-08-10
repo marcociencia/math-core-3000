ea4c106f85bde4ecf8ac560053533130481f522f
Marco Aurélio Barbosa do Santos <55638752+marcociencia@users.noreply.github.com>
2026-08-09 22:01:18 -0300

    Rename package to 'calc_backend' and update version
    Updated the package name and version for the calculator extension.

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup, Extension
 
 class get_pybind_include(object):
-    """Classe auxiliar para atrasar a importação do pybind11 até que ele esteja instalado na nuvem."""
     def __str__(self):
         import pybind11
         return pybind11.get_include()
 
 ext_modules = [
     Extension(
-        'calculator_engine',
+        'calc_backend',
         ['calculator.cpp'],
         include_dirs=[get_pybind_include()],
         language='c++',
@@ -17,7 +16,7 @@ ext_modules = [
 ]
 
 setup(
-    name='calculator_engine',
-    version='0.1',
+    name='calc_backend',
+    version='1.0',
     ext_modules=ext_modules,
 )
