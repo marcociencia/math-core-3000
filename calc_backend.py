266df99c94c070f81416f225fbf244dc9986b831
Marco Aurélio Barbosa do Santos <55638752+marcociencia@users.noreply.github.com>
2026-08-10 01:17:06 -0300

    Delete calc_backend.py

--- calc_backend.py
+++
@@ -1,8 +0,0 @@
-class AdvancedCalculator:
-    def add(self, a, b): return a + b
-    def subtract(self, a, b): return a - b
-    def multiply(self, a, b): return a * b
-    def divide(self, a, b):
-        if b == 0: raise ZeroDivisionError("Division by zero!")
-        return a / b
-    def power(self, a, b): return a ** b
