773d695b69ea921124a8d6bddb835d61c6472bee
Marco Aurélio Barbosa do Santos <55638752+marcociencia@users.noreply.github.com>
2026-08-10 01:22:14 -0300

    Add square root, percentage, and factorial methods

--- calculator.cpp
+++ calculator.cpp
@@ -1,6 +1,7 @@
 #include <pybind11/pybind11.h>
 #include <stdexcept>
 #include <cmath>
+#include <string>
 
 namespace py = pybind11;
 
@@ -14,6 +15,18 @@ public:
         return a / b; 
     }
     double power(double base, double exp) { return std::pow(base, exp); }
+    double sqrt_num(double a) { 
+        if (a < 0) throw std::invalid_argument("Cannot calculate square root of negative number!");
+        return std::sqrt(a); 
+    }
+    double percentage(double a, double b) { return (a * b) / 100.0; }
+    long long factorial(int n) {
+        if (n < 0) throw std::invalid_argument("Factorial of negative number!");
+        if (n > 20) throw std::invalid_argument("Number too large!");
+        long long result = 1;
+        for(int i = 2; i <= n; i++) result *= i;
+        return result;
+    }
 };
 
 PYBIND11_MODULE(calc_backend, m) {
@@ -23,5 +36,8 @@ PYBIND11_MODULE(calc_backend, m) {
         .def("subtract", &AdvancedCalculator::subtract)
         .def("multiply", &AdvancedCalculator::multiply)
         .def("divide", &AdvancedCalculator::divide)
-        .def("power", &AdvancedCalculator::power);
+        .def("power", &AdvancedCalculator::power)
+        .def("sqrt", &AdvancedCalculator::sqrt_num)
+        .def("percentage", &AdvancedCalculator::percentage)
+        .def("factorial", &AdvancedCalculator::factorial);
 }
