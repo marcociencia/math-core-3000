488cba6cc0a003907d8620ee562a19671aeb4c6b
Marco Aurélio Barbosa do Santos <55638752+marcociencia@users.noreply.github.com>
2026-08-10 01:05:43 -0300

    Update setup.py to include dependencies and optimizations
    Added install_requires for pybind11 and optimization flag.

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 from setuptools import setup, Extension
-
-class get_pybind_include(object):
-    def __str__(self):
-        import pybind11
-        return pybind11.get_include()
+import pybind11
 
 ext_modules = [
     Extension(
         'calc_backend',
         ['calculator.cpp'],
-        include_dirs=[get_pybind_include()],
+        include_dirs=[pybind11.get_include()],
         language='c++',
-        extra_compile_args=['-std=c++17'],
+        extra_compile_args=['-std=c++17', '-O2'],
     ),
 ]
 
@@ -19,4 +15,5 @@ setup(
     name='calc_backend',
     version='1.0',
     ext_modules=ext_modules,
+    install_requires=['pybind11>=2.6.0'],
 )
